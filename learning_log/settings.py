e23e49d76374ef69844e6c79ad282f1b53729b1c
missmelba <missymae76@gmail.com>
2017-03-16 09:29:36 -0700

    READY for Heroku: commented heroku database var; added try/except for local module to fix ModuleNotFoundError

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import local
+
+try:
+    import local
+except ModuleNotFoundError:
+    pass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -36,12 +40,12 @@ ALLOWED_HOSTS = local.allowed_hosts()
 DATABASES = local.databases(BASE_DIR)
 
 # Settings for Heroku
-if os.getcwd() == '/app':
-    import dj_database_url
+# if os.getcwd() == '/app':
+#     import dj_database_url
 
-    DATABASES = {
-        'default': dj_database_url.config(default='postgres://localhost')
-    }
+#     DATABASES = {
+#         'default': dj_database_url.config(default='postgres://localhost')
+#     }
 
     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
     # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
