c0ec4d83b4103210ff9574b7e51b0c340fc4725d
missmelba <missymae76@gmail.com>
2017-03-15 18:45:49 -0700

    READY for Heroku: uncommented DATABASES setting in Settings for Heroku in settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -145,9 +145,9 @@ BOOTSTRAP3 = {
 if os.getcwd() == '/app':
 #     import dj_database_url
 
-#     DATABASES = {
-#         'default': dj_database_url.config(default='postgres://localhost')
-#     }
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
 
     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
