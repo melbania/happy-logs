00937cfc942f71ed5d77b3b08a2c044cb87cc3f5
Melissa Baker <melissa@AbsurdAirBabboon.local>
2019-03-05 15:49:40 -0700

    update learning_log/urls.py

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^users/', include('users.urls', namespace='users')),
-    url(r'', include('learning_logs.urls', namespace='learning_logs')),
+
+    url(r'^users/', include('users.urls')),
+    url(r'', include('learning_logs.urls')),
 ]
