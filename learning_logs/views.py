894b2b50fd30bbdbde243ab3902134ffbe185b0f
Melissa Baker <melissa@AbsurdAirBabboon.local>
2019-03-05 15:55:55 -0700

    update learning_logs/views.py and users/views.py

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.db.models.functions import Lower
 from django.utils import timezone
