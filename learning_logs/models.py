b4d7ca6cf88f766d9d5b041839ac09f5717a6808
Melissa Baker <melissa@AbsurdAirBabboon.local>
2019-03-05 14:18:24 -0700

    Update models.py

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -8,7 +8,7 @@ class Topic(models.Model):
     """A topic the user is learning about"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a string representation of the model."""
@@ -16,7 +16,7 @@ class Topic(models.Model):
 
 class Entry(models.Model):
     """Something specific learned about a topic."""
-    topic = models.ForeignKey(Topic)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     entry = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
     
