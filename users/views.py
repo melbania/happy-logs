9718892919e49e92e16e4cf502a2652422841b76
Melissa Baker <melissa@AbsurdAirBabboon.local>
2019-03-05 15:30:40 -0700

    Update users/views.py

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+# from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
