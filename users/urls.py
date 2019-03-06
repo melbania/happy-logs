94e48cb5d123f8a7e4c922481ef54cbe45de9e65
Melissa Baker <melissa@AbsurdAirBabboon.local>
2019-03-05 17:04:57 -0700

    update users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 """Defines url patterns for users."""
-from django.conf.urls import url
+from django.conf.urls import path
 from django.contrib.auth import views as auth_views
 
 from . import views
@@ -10,11 +10,11 @@ urlpatterns = [
     # Login page
     # url(r'^login/$', login, {'template_name': 'users/login.html'}, 
         # name='login'),
-    url(r'^login$', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path(r'^login$', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     
     # Logout page
-    url(r'^logout/$', views.logout_view, name='logout'),
+    path(r'^logout/$', views.logout_view, name='logout'),
 
     # Registration page
-    url(r'^register/$', views.register, name='register'),
+    path(r'^register/$', views.register, name='register'),
 ]
